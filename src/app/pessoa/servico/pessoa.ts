363e2a94fca8195e38b56a1b8c8c64fc7cff107c
Genival Soares Barbosa Filho <gbarbosa@procenge.com.br>
2020-05-07 18:16:37 -0300

    aula_06_05

---
+++ src/app/pessoa/servico/pessoa.ts
@@ -0,0 +1,11 @@
+
+export class Pessoa  {    
+    codigo: string = ''
+    nome: string = '';
+    telefone: string = '';
+    dataNascimento: string = '';
+    endereco: string = '';
+    bairro: string = '';
+    cidade: string = '';
+    estado: string = '';      
+}
