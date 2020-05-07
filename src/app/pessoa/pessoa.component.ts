d4813532f8bf4c2af8a1f4e685b81ce71a990350
Genival Soares Barbosa Filho <gbarbosa@procenge.com.br>
2020-05-07 18:16:13 -0300

    aula_06_05

--- src/app/pessoa/pessoa.component.ts
+++ src/app/pessoa/pessoa.component.ts
@@ -1,38 +1,60 @@
 import { Component, OnInit } from '@angular/core';
+import { Pessoa } from './servico/pessoa';
 
 @Component({
   selector: 'app-pessoa',
   templateUrl: './pessoa.component.html',
   styleUrls: ['./pessoa.component.css']
 })
-export class PessoaComponent implements OnInit {
+export class PessoaComponent implements OnInit {  
 
   titulo: string = 'Cadastro titulo';
 
-  codigo: string = ''
-  nome: string = 'Genival';
-  telefone: string = '';
-  dataNascimento: string = '';
-  endereco: string = '';
-  bairro: string = '';
-  cidade: string = '';
-  estado: string = '';
+  pessoa: Pessoa = new Pessoa();
+
+  listaPessoas: Pessoa[] = [
+    {codigo: '1' ,nome: 'Genival', telefone: '9999' , 
+      dataNascimento: '99/99/99', endereco: 'avenida', bairro: 'bairro', 
+      cidade: 'cidade', estado: 'estado'},
+    {codigo: '2' ,nome: 'Jose', telefone: '9999' , 
+      dataNascimento: '99/99/99', endereco: 'avenida', bairro: 'bairro', 
+      cidade: 'cidade', estado: 'estado'}
+
+  ];
+
+  listaEstado: any[] = [
+    {codigo:'PE', nome: 'Pernambuco'},
+    {codigo:'AL', nome: 'Alagoas'},
+    {codigo:'MA', nome: 'Maranhão'}
+  ];
+
+  mostrarCadastroPessoa: boolean = false;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  imprimir(){    
-    alert(`${this.codigo}
-    ${this.nome}
-    ${this.telefone}
-    ${this.dataNascimento}
-    ${this.endereco}
-    ${this.bairro}
-    ${this.cidade}
-    ${this.estado}
+  incluir(){
+    this.listaPessoas.push(this.pessoa);
+  }
+
+  imprimir(){ 
+
+    alert(`${this.pessoa.codigo}
+    ${this.pessoa.nome}
+    ${this.pessoa.telefone}
+    ${this.pessoa.dataNascimento}
+    ${this.pessoa.endereco}
+    ${this.pessoa.bairro}
+    ${this.pessoa.cidade}
+    ${this.pessoa.estado}
     `);
+  }
+
+  
+  cadastroPessoa(){
+    this.mostrarCadastroPessoa = !this.mostrarCadastroPessoa;
   }
 
 }
